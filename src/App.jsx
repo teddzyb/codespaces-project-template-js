ad9cae0a07ea4a34e309ec3764f3fa8adebf0885
Edwin Bartlett <67263400+teddzyb@users.noreply.github.com>
2025-04-27 09:38:06 +0000

    Update siteProps with personal information and add hover animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Edwin Bartlett",
+  title: "Software Developer",
+  email: "ebartlett@fullscale.ph",
+  gitHub: "https://github.com/teddzyb",
+  instagram: "https://instagram.com/teddzyb",
+  linkedIn: "https://linkedin.com/teddzyb",
+  medium: "https://medium.com/teddzyb",
+  twitter: "https://twitter.com/teddzyb",
+  youTube: "https://youtube.com/teddzyb",
 };
 
 const primaryColor = "#4E567E";
